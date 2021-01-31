5f5ce647314ffa59023824d1d850dd9868cdf2d2
FreyaP <65002708+FreyaP@users.noreply.github.com>
2021-01-30 19:17:13 -0800

    Changed nav color on scroll

--- GB Contracting/src/GB.js
+++ GB Contracting/src/GB.js
@@ -1,12 +1,20 @@
 window.onscroll = () => { scrollFunction()};
 
+
+
 function scrollFunction() {
-    
+    var links = document.getElementsByClassName("navLink");
+    var i;
+
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 80) {
         document.getElementById("header").style.backgroundColor = "rgb(255, 255, 255)";
-        document.getElementById("nav-items").style.color = "blue";
+        for (i = 0; i < links.length; i++) {
+            links[i].style.color = "#272D31";
+        }
     } else {
         document.getElementById("header").style.backgroundColor = "rgba(255, 255, 255, 0)";
-        document.getElementById("nav-items").style.color = "white";
+        for (i = 0; i < links.length; i++) {
+            links[i].style.color = "white";
+        }
     }
 }
