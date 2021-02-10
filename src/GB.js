9bbfb8e86f0a3a3201dff4f11544d7e14ca3e7e5
FreyaP <65002708+FreyaP@users.noreply.github.com>
2021-02-09 22:47:06 -0800

    Added logo - adjusting palette

--- src/GB.js
+++ src/GB.js
@@ -7,12 +7,14 @@ function scrollFunction() {
     var i;
 
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 80) {
-        document.getElementById("header").style.backgroundColor = "rgb(255, 255, 255)";
+        document.getElementById("header").style.backgroundColor = "rgb(255, 255, 255, 0.8)";
+        document.getElementById("header-img").style.width = "3rem";
         for (i = 0; i < links.length; i++) {
             links[i].style.color = "#272D31";
         }
     } else {
         document.getElementById("header").style.backgroundColor = "rgba(255, 255, 255, 0)";
+        document.getElementById("header-img").style.width = "5rem";
         for (i = 0; i < links.length; i++) {
             links[i].style.color = "white";
         }
