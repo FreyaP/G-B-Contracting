e39e327f7593dff5e9aad08bc1edc5243d06d942
FreyaP <65002708+FreyaP@users.noreply.github.com>
2021-02-12 10:17:24 -0800

    Hamburger button

--- src/GB.js
+++ src/GB.js
@@ -1,7 +1,5 @@
 window.onscroll = () => { scrollFunction()};
 
-
-
 function scrollFunction() {
     var links = document.getElementsByClassName("navLink");
     var i;
@@ -12,11 +10,21 @@ function scrollFunction() {
         for (i = 0; i < links.length; i++) {
             links[i].style.color = "#272D31";
         }
+        hamburger.style.color = "#272D31";
     } else {
         document.getElementById("header").style.backgroundColor = "rgba(255, 255, 255, 0)";
         document.getElementById("header-img").style.width = "5rem";
         for (i = 0; i < links.length; i++) {
             links[i].style.color = "white";
         }
+        hamburger.style.color = "white";
     }
 }
+
+const hamburger = document.querySelector('button[aria-expanded]');
+function toggleNav({ target }) {
+    const expanded = target.getAttribute('aria-expanded') === 'true' || false;
+    hamburger.setAttribute('aria-expanded', !expanded);
+}
+
+hamburger.addEventListener('click', toggleNav);
